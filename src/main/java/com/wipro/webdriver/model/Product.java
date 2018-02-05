94a8f6b97f02d9b3ea2907ef0cc3bf0482489862
Kumar Saurav (BAS) <KU395948@wipro.com>
2018-02-05 12:35:02 +0530

    First commit

---
+++ src/main/java/com/wipro/webdriver/model/Product.java
@@ -0,0 +1,17 @@
+package com.wipro.webdriver.model;
+
+public class Product {
+    private String productQty;
+
+    public Product(String productQty) {
+        this.productQty = productQty;
+    }
+    public String getProductQty() {
+        return productQty;
+    }
+
+    public void setProductQty(String productQty) {
+        this.productQty = productQty;
+    }
+
+}
